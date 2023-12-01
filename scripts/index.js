cf35c2fd1e540761b3e83200fda9d84773df6463
Dalton <97250252+dalton-f@users.noreply.github.com>
2023-12-01 17:46:50 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,16 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // Helper Functions
-getById = (id) => document.getElementById(id);
-getByClassName = (className) => document.getElementsByClassName(className);
+getElementById = (id) => document.getElementById(id);
+getElementByClassName = (className) =>
+  document.getElementsByClassName(className);
 
 // Global variables
 var playerOne = false;
 var list = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8"];
 var gameStarted = false;
-var indicator = getById("indicator");
-var reset = getById("reset");
+var indicator = getElementById("indicator");
+var reset = getElementById("reset");
 var allowed = [false, false, false, false, false, false, false, false, false];
 
 function setupPlayers() {
@@ -21,14 +22,14 @@ function setupPlayers() {
 
 async function startGame() {
   gameStarted = true;
-  getByClassName("StartGamebt")[0].style.display = "none";
+  getElementByClassName("StartGamebt")[0].style.display = "none";
   // Reseting the board
   for (let i of list) {
-    getById(i).innerHTML = "\xA0";
+    getElementById(i).innerHTML = "\xA0";
     await sleep(200);
   }
   // Destroy StartGamebg
-  getByClassName("StartGamebg")[0].style.display = "none";
+  getElementByClassName("StartGamebg")[0].style.display = "none";
   indicator.style.display = "block";
   setupPlayers();
 }
@@ -38,65 +39,65 @@ function checkWin() {
   var winner = "";
   // Horizontal
   if (
-    getById(list[0]).innerText == getById(list[1]).innerText &&
-    getById(list[1]).innerText == getById(list[2]).innerText &&
-    getById(list[0]).innerText != ""
+    getElementById(list[0]).innerText == getElementById(list[1]).innerText &&
+    getElementById(list[1]).innerText == getElementById(list[2]).innerText &&
+    getElementById(list[0]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[0]).innerText;
+    winner = getElementById(list[0]).innerText;
   } else if (
-    getById(list[3]).innerText == getById(list[4]).innerText &&
-    getById(list[4]).innerText == getById(list[5]).innerText &&
-    getById(list[3]).innerText != ""
+    getElementById(list[3]).innerText == getElementById(list[4]).innerText &&
+    getElementById(list[4]).innerText == getElementById(list[5]).innerText &&
+    getElementById(list[3]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[3]).innerText;
+    winner = getElementById(list[3]).innerText;
   } else if (
-    getById(list[6]).innerText == getById(list[7]).innerText &&
-    getById(list[7]).innerText == getById(list[8]).innerText &&
-    getById(list[6]).innerText != ""
+    getElementById(list[6]).innerText == getElementById(list[7]).innerText &&
+    getElementById(list[7]).innerText == getElementById(list[8]).innerText &&
+    getElementById(list[6]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[6]).innerText;
+    winner = getElementById(list[6]).innerText;
   }
   // Vertical
   else if (
-    getById(list[0]).innerText == getById(list[3]).innerText &&
-    getById(list[3]).innerText == getById(list[6]).innerText &&
-    getById(list[0]).innerText != ""
+    getElementById(list[0]).innerText == getElementById(list[3]).innerText &&
+    getElementById(list[3]).innerText == getElementById(list[6]).innerText &&
+    getElementById(list[0]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[0]).innerText;
+    winner = getElementById(list[0]).innerText;
   } else if (
-    getById(list[1]).innerText == getById(list[4]).innerText &&
-    getById(list[4]).innerText == getById(list[7]).innerText &&
-    getById(list[1]).innerText != ""
+    getElementById(list[1]).innerText == getElementById(list[4]).innerText &&
+    getElementById(list[4]).innerText == getElementById(list[7]).innerText &&
+    getElementById(list[1]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[1]).innerText;
+    winner = getElementById(list[1]).innerText;
   } else if (
-    getById(list[2]).innerText == getById(list[5]).innerText &&
-    getById(list[5]).innerText == getById(list[8]).innerText &&
-    getById(list[2]).innerText != ""
+    getElementById(list[2]).innerText == getElementById(list[5]).innerText &&
+    getElementById(list[5]).innerText == getElementById(list[8]).innerText &&
+    getElementById(list[2]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[2]).innerText;
+    winner = getElementById(list[2]).innerText;
   }
   // Diagonal
   else if (
-    getById(list[0]).innerText == getById(list[4]).innerText &&
-    getById(list[4]).innerText == getById(list[8]).innerText &&
-    getById(list[0]).innerText != ""
+    getElementById(list[0]).innerText == getElementById(list[4]).innerText &&
+    getElementById(list[4]).innerText == getElementById(list[8]).innerText &&
+    getElementById(list[0]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[0]).innerText;
+    winner = getElementById(list[0]).innerText;
   } else if (
-    getById(list[2]).innerText == getById(list[4]).innerText &&
-    getById(list[4]).innerText == getById(list[6]).innerText &&
-    getById(list[2]).innerText != ""
+    getElementById(list[2]).innerText == getElementById(list[4]).innerText &&
+    getElementById(list[4]).innerText == getElementById(list[6]).innerText &&
+    getElementById(list[2]).innerText != ""
   ) {
     win = true;
-    winner = getById(list[2]).innerText;
+    winner = getElementById(list[2]).innerText;
   }
   // Check if its filled
   else if (allowed.every((val, i, arr) => val === true)) {
